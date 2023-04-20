3a896a79c39790a3e4656a4d3b75ce312f129d16
unknown <Efrain>
2023-04-20 10:14:28 -0400

    ahora el factorial fue modificado

--- Factorial.py
+++ Factorial.py
@@ -1,9 +1,10 @@
 def factorial(n):
     if(n == 0):
-        return 1
+        res = 1
     elif(n == 1):
-        return 1
+        res = 1
     else:
-        return n*factorial(n - 1)
-
+        res = n*factorial(n - 1)
+    return res
+    
 print(factorial(5))
